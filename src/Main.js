bfeb512b33e5ccc8ee2e36a7fb86f24a26aa8661
FlippahOrtega <felipe.ortegalugo@gmail.com>
2021-05-17 02:51:46 +0000

    here maps

--- src/Main.js
+++ src/Main.js
@@ -6,11 +6,11 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class Main() extends Component {
+class Main extends Component {
   render() {
     return (
       <div className="App">
-        
+        hola
       </div>
     );
   }
