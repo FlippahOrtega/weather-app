79b1b5898e8c53ce654eb27db6b8943a1e6252d7
FlippahOrtega <felipe.ortegalugo@gmail.com>
2021-05-17 02:34:11 +0000

    here maps

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-import {DisplayMapClass} from './DisplayMapClass';
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -12,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         hola como estan
-          <DisplayMapClass />
+         
       </div>
     );
   }
