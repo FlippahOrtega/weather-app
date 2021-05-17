59e59aa23d6844a7e1267af62b1128e8805f45f1
FlippahOrtega <felipe.ortegalugo@gmail.com>
2021-05-17 02:45:26 +0000

    here maps

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+
+//import {DisplayMapClass} from './DisplayMapClass';
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+        hola como estan
+         
+      </div>
+    );
+  }
+}
+export default Main;
