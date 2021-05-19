4dbe14f67827f87a60aef8ff64c767480ec29e54
flippah <flippah@live.com>
2021-05-18 23:07:21 -0500

    add backend amplify

--- src/Main.js
+++ src/Main.js
@@ -6,13 +6,28 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class Main extends Component {
-  render() {
-    return (
-      <div className="App">
-        hola otra prueba  un nuevo deploy
-      </div>
-    );
-  }
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			console.log('Latitude is :', position.coords.latitude);
+			console.log('Longitude is :', position.coords.longitude);
+
+			var requestOptions = {
+				method: 'GET',
+				redirect: 'follow',
+			};
+
+			fetch(
+				`https://tmb3ecnknb.execute-api.us-east-1.amazonaws.com/Prod/weather/api/weather/1.0/report.json?product=observation&latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&oneobservation=true`,
+				requestOptions
+			)
+				.then((response) => response.text())
+				.then((result) => console.log('resultt', result))
+				.catch((error) => console.log('error', error));
+		});
+	}
+	render() {
+		return <div className="App">hola otra prueba un nuevo deploy</div>;
+	}
 }
 
 export default Main;
